d5c2613a6c2d846fd34561a3f50a233f96efd9ca
Konrad Kohbrok <konrad.kohbrok@datashrine.de>
2025-08-08 09:24:23 +0200

    storage traits and ID for HpqSignatureKeyPair

--- src/authentication.rs
+++ src/authentication.rs
@@ -4,6 +4,7 @@
 
 use openmls::prelude::{BasicCredential, CredentialWithKey, SignaturePublicKey, SignatureScheme};
 use openmls_basic_credential::SignatureKeyPair;
+use openmls_traits::storage::{self, CURRENT_VERSION};
 use serde::{Deserialize, Serialize};
 use tls_codec::{TlsDeserialize, TlsSerialize, TlsSize};
 
@@ -78,7 +79,7 @@ impl From<HpqCiphersuite> for HpqSignatureScheme {
     }
 }
 
-#[derive(Debug, Clone, TlsSize, TlsSerialize, TlsDeserialize)]
+#[derive(Debug, Clone, TlsSize, TlsSerialize, TlsDeserialize, Serialize, Deserialize)]
 pub struct HpqSignatureKeyPair {
     pub t_signer: SignatureKeyPair,
     pub pq_signer: SignatureKeyPair,
@@ -93,6 +94,15 @@ impl HpqSignatureKeyPair {
             pq_signer,
         }
     }
+
+    pub fn id(&self) -> HpqStorageId {
+        HpqStorageId {
+            t_signature_scheme: self.t_signer().signature_scheme(),
+            t_verifying_key: self.t_verifying_key().to_vec(),
+            pq_signature_scheme: self.pq_signer().signature_scheme(),
+            pq_verifying_key: self.pq_verifying_key().to_vec(),
+        }
+    }
 }
 
 impl HpqSigner for HpqSignatureKeyPair {
@@ -104,3 +114,19 @@ impl HpqSigner for HpqSignatureKeyPair {
         &self.pq_signer
     }
 }
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct HpqStorageId {
+    pub t_signature_scheme: SignatureScheme,
+    pub t_verifying_key: Vec<u8>,
+    pub pq_signature_scheme: SignatureScheme,
+    pub pq_verifying_key: Vec<u8>,
+}
+
+// Implement key traits for the storage id
+impl storage::Key<CURRENT_VERSION> for HpqStorageId {}
+impl storage::traits::SignaturePublicKey<CURRENT_VERSION> for HpqStorageId {}
+
+// Implement entity trait for the signature key pair
+impl storage::Entity<CURRENT_VERSION> for HpqSignatureKeyPair {}
+impl storage::traits::SignatureKeyPair<CURRENT_VERSION> for HpqSignatureKeyPair {}
